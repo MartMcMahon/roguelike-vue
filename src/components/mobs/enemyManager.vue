ab542252297e73c0406472951e97aca0751b9f98
mart <matt@paradigmautomation.co>
2018-07-20 16:52:44 -0500

    moved Enemy to it's own class

--- src/components/mobs/enemyManager.vue
+++ src/components/mobs/enemyManager.vue
@@ -19,10 +19,7 @@ export default {
 	},
 	computed: mapGetters(['enemies']),
 	created() {
-		// let e = this.defaultEnemy
 		let e = new Enemy()
-		// e.pos = [8, 1]
-		// e.key = 1
 		console.log(e)
 		this.addEnemy(e)
 		e = new Enemy()
@@ -37,41 +34,7 @@ export default {
 		// console.log(this.$store.dispatch('testAction'))
 	},
 	data() {
-		return {
-			defaultEnemy: {
-				key: 1, 
-				pos: [8, 1],
-				getCoordsStr() {
-					return this.pos[0] + ',' + this.pos[1]
-				},
-				move() {
-					let dir = Math.floor(Math.random() * 10) % 4
-					switch (dir) {
-						case 0:
-							this.pos[1]--
-							break
-						case 1:
-							this.pos[1]++
-							break
-						case 2:
-							this.pos[0]++
-							break
-						case 3:
-							this.pos[0]--
-							break
-					}
-					return Object.assign({}, this)
-				},
-				die() {
-					//drop loot
-					//add loot to map
-
-					//explode?
-					//trigger onDeath event
-					//trigger onKill event
-				},
-			}
-		}
+		return {}
 	},
 	methods: {
 		addEnemy(enemy) {
