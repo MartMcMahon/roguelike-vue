5b7e799dbe2ae58635dac73069cfb982a655f573
mart <matt@paradigmautomation.co>
2018-07-22 15:44:12 -0500

    cleaned types

--- src/types/vector.ts
+++ src/types/vector.ts
@@ -1,6 +1,6 @@
-import { Direction } from './types'
+import { Direction } from '@/types'
 
-export default class Vector {
+export default class Vector extends Array {
 	x: number = 0
 	y: number = 0
 	dir?: Direction = 0
@@ -13,14 +13,23 @@ export default class Vector {
 		dir: Direction = 0,
 		velocity: number = 0) {
 			
+		super()
 		if (pos) {
-			this.x = pos[0]
-			this.y = pos[1]
+			this[0] = pos[0]
+			this[1] = pos[1]
 		}
+	}
+
+	static add(a: Vector, b: Vector): Vector {
+		let x = a[0] + b[0]
+		let y = a[1] + b[1]
+		return new Vector(x, y)
 	}
 
 	static UP = new Vector(0, -1, undefined, Direction.up)
 	static RIGHT = new Vector(1, 0, undefined, Direction.right)
 	static DOWN = new Vector(0, 1, undefined, Direction.down)
 	static LEFT = new Vector(-1, 0, undefined, Direction.left)
+
+	static dirs = [Vector.UP, Vector.RIGHT, Vector.DOWN, Vector.LEFT]
 }
