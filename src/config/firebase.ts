e1f36b48d36e0935b76f407f7a647a9f8e1d1094
MartMcMahon <mattthemcman@gmail.com>
2018-08-05 00:13:54 -0500

    最初

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,6 @@
 export const config = {
-	apiKey: 'AIzaSyBwOzTs2VX7cYRGk3_pKIcRuSqJdrP9e70',
-	authDomain: 'roguelike-vue.firebaseapp.com',
+	apiKey: '*',
+	authDomain: '*',
 	databaseURL: 'https://roguelike-vue.firebaseio.com',
 	projectId: 'roguelike-vue',
 	storageBucket: 'roguelike-vue.appspot.com',
