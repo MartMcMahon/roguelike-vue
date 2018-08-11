671477fef893930594ff7b801ff6295dc4d48b40
mart <themcman@gmail.com>
2018-08-11 14:49:54 -0500

    notification settings debug

--- src/components/subscribeButton.vue
+++ src/components/subscribeButton.vue
@@ -20,7 +20,7 @@ export default class SubscribeButton extends Vue {
 	created() {
 		this.messaging.usePublicVapidKey("BM47ty_fAI2_Kd0AH0gUy3KtbTs1AMkqozdNmCsMcIOZNpEafunPf7ozMO0tA41hiWqcPMEvIvJR914zvd17UYA")
 
-		this.messaging.requestPermission()
+		this.subscribe()
 
 		this.messaging.onTokenRefresh( () => {
 			this.messaging.getToken().then( (refreshedToken) => {
@@ -43,9 +43,10 @@ export default class SubscribeButton extends Vue {
 		return this.$store.state.user
 	}
 
-	onClick() {
+	subscribe() {
 		this.messaging.requestPermission()
-			.then( () => {
+			.then( (perm) => {
+				console.log(perm)
 				this.messaging.getToken()
 					.then( (token) => {
 						console.log(token)
@@ -64,80 +65,4 @@ export default class SubscribeButton extends Vue {
 			})
 	}
 }
-
-							/*
-							console.log('writing token to firestore')
-							const userDoc = firestore.collection('users').doc(this.user.uid)
-							userDoc.get().then( (doc) => {
-								let data = doc.data()
-								// make sure data for this user exists
-								if (data && data.notificationTokensArray) {
-									let res = data.notificationTokensArray
-									// if this token is already listed, return null and do nothing
-									if (token in res) {
-										console.log(res)
-										return res
-									}
-									// if this is a new token, write that shit to the array
-									else {
-										res.push(token)
-										return res
-									}
-								}
-								// array gets returned from last one only if we add to it.
-								// this then sets the new array to firestore
-							}).then( (res) => {
-								if (res != null) {
-									userDoc.set({ notificationTokensArray: res })
-								}
-							})
-  					}
-				})
-				// if we can't get a token, we are probs running locally
-				.catch((err) => {
-					// Use err to check if blocked.
-					console.log('couldn\'t get token. You\'re probably running in dev mode.')
-					console.log('using "Test Token" ')
-
-					const userDoc = firestore.collection('users').doc(this.user.uid)
-					userDoc.get().then( (doc) => {
-						let data = doc.data()
-						if (data && data.notificationTokensArray) {
-							let res = data.notificationTokensArray
-							if ("Test Token" in res) {
-								console.log(res)
-								return res
-							}
-							else {
-								res.push("Test Token")
-								return res
-							}
-						}
-					})
-					.then( (res) => {
-						if (res != null) {
-							userDoc.set({ notificationTokensArray: res })
-						}
-					})
-
-					/** object token field
-					firestore.collection('users').doc(this.user.uid).get().then( (doc) => {
-						let data = doc.data()
-						if (data) {
-							let res = {...data.notificationTokens, "Test Token": "Sunday" }
-							console.log(res)
-							return res
-						}
-					}).then( (newTokens) => {
-						firestore.collection('users').doc(this.user.uid).set({
-							notificationTokens: newTokens
-						}, { merge: true })
-					})
-
-					**/
-// 				})
-// 		})
-// 	}
-// }
-
 </script>
