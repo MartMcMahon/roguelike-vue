25e9c9e35b1ca9030a93baf8b3e41b2e38f11594
mart <themcman@gmail.com>
2018-08-11 11:40:58 -0500

    added cloud functions

---
+++ firebase-moontowergames/functions/lib/index.js
@@ -0,0 +1,70 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+// import { DeltaSnapshot } from 'firebase-functions/lib/providers/database';
+// import { Change } from '../node_modules/firebase-functions/lib/cloud-functions';
+// init admin
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const buildPayload = (context) => {
+    return {
+        data: {
+            data_type: "direct_message",
+            title: "New Message: " + context.params.msgId,
+            message: context.params.msgBody,
+            message_id: 'ok'
+        }
+    };
+};
+exports.sendMessageNotification = functions.firestore.document('messages/{msgId}')
+    .onWrite((change, context) => {
+    console.log(change);
+    console.log(context);
+    const payload = buildPayload(context);
+    return db.collection('users').doc('{userId}').collection('notificationTokens').get()
+        .then((notificationTokens) => {
+        if (notificationTokens) {
+            notificationTokens.forEach(doc => {
+                console.log('sent a message');
+                return admin.messaging().sendToDevice(doc.id, payload);
+            });
+        }
+    });
+    return new Promise(() => 'all set');
+    // console.log(getDeviceTokensPromise)
+    //get the user id of the person who sent the message
+    // const sender = change.after.child('sender').val();
+    // const sender = 'me'
+    // console.log("sender: ", sender);
+    // const fcmToken = 'fXfkpMKvcnc:APA91bG-F6WMwNSg3zLKPuyiDrJ06n7TjOVz3Ym6JAeb-oX-Cxd7ATUljRJGwn8ocTzvRtDW-GgFoCnjJqSh0iq28gq5g1Q5oEDFrygYgq5o-1f4TuZXuFdJvFBweG9XjE9EW6K0koXA'
+    // Build the message payload and send the message
+    // console.log("Construction the notification message.");
+    // const payload = {
+    // 	data: {
+    // 		data_type: "direct_message",
+    // 		title: "New Message: " + context.params.msgId,//from " + sender,
+    // 		message: "YO",
+    // 		message_id: 'ok',
+    // 	}
+    // };
+    // return admin.messaging().sendToDevice(fcmToken, payload)
+    // 	.then(function(response) {
+    // 		console.log("Successfully sent message:", response);
+    // 	})
+    // 	.catch(function(error) {
+    // 		console.log("Error sending message:", error);
+    // 	});
+});
+// exports.sendMessageNotification = functions.database.ref('/messages/{msgId}')
+// 	.onWrite( (change, context) => {
+// 		console.log(change)
+// 		console.log(context)
+// 		console.log(context)
+// 		// Get the list of device notification tokens.
+// 		const getDeviceTokensPromise = admin.database()
+// 		.ref('/users/${userId}/notificationTokens').once('value');
+// 		console.log(getDeviceTokensPromise)
+// // Get the follower profile.
+// const getFollowerProfilePromise = admin.auth().getUser(userId);
+//# sourceMappingURL=index.js.map
